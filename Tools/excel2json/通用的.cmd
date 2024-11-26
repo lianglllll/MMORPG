42410f2e893b367777e05185f724702d1606d288
xiaoliangba <2528201418@qq.com>
2024-11-26 23:38:01 +0800

    [优化]execl工具优化

--- Tools/excel2json/通用的.cmd
+++ Tools/excel2json/通用的.cmd
@@ -8,11 +8,8 @@
 @SET DEST_FOLDER_2=..\..\MMO-SERVER\GameServer\bin\Release\net6.0\Data
 @SET DEST_FOLDER_3=..\..\MMO-SERVER\GameServer\bin\Debug\net6.0\Data
 :: define文件要复制到的目的地
-<<<<<<< HEAD
 @SET DEST_FOLDER_4=..\..\MMO-Client\MMOGame\Assets\Scripts\Manager\DataDefine
-=======
 @SET DEST_FOLDER_4=..\..\MMO-Client\MMOGame\Assets\Script\Manager\DataDefine
->>>>>>> 71706afc428a8a0d57234e05ead5afba5bf04b94
 @SET DEST_FOLDER_5=..\..\MMO-SERVER\GameServer\DataDefine
 
 :: execl转json和生成对应的define文件
@@ -22,10 +19,6 @@ for %%i in ("%EXCEL_FOLDER%\*.xlsx") do (
     @CALL %EXE% --excel "%%i" --json "%JSON_FOLDER%\%%~ni.json" --csharp "%CODE_FOLDER%\%%~ni.cs" --header 3 --exclude_prefix #
 )
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 71706afc428a8a0d57234e05ead5afba5bf04b94
 :: 复制json文件
 @ECHO Copying JSON files to destination folder %DEST_FOLDER% ...
 for %%i in (%JSON_FOLDER%\*.json) do (
@@ -36,11 +29,9 @@ for %%i in (%JSON_FOLDER%\*.json) do (
 )
 
 :: 复制define文件
-<<<<<<< HEAD
+
 for %%i in (%CODE_FOLDER%\*.cs) do (
-=======
 for /r %CODE_FOLDER% %%i in (*.cs) do (
->>>>>>> 71706afc428a8a0d57234e05ead5afba5bf04b94
     @echo   copying %%~nxi 
     @COPY "%%i" "%DEST_FOLDER_4%\%%~nxi"
     @COPY "%%i" "%DEST_FOLDER_5%\%%~nxi"
