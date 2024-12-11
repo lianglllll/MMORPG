80b944aefa01b8ea1ea1660ca7dd62fe7392dfea
大衍神君且刮风 <2528201418@qq.com>
2024-12-11 21:01:55 +0800

    [添加]chunk文件的解析

---
+++ MMO-SERVER/Llua/binchunk/Binary_Chunk.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lLua.binchunk
+{
+    public static class LuaConstants
+    {
+        public const string LUA_SIGNATURE = "\x1bLua";
+        public const byte LUAC_VERSION = 0x53;
+        public const byte LUAC_FORMAT = 0;
+        public static readonly byte[] LUAC_DATA = { 0x19, 0x93, 0x0D, 0x0A, 0x1A, 0x0A };
+        public const int CINT_SIZE = 4;
+        public const int CSIZET_SIZE = 4; 
+        public const int INSTRUCTION_SIZE = 4;
+        public const int LUA_INTEGER_SIZE = 8;
+        public const int LUA_NUMBER_SIZE = 8;
+        public const int LUAC_INT = 0x5678;
+        public const double LUAC_NUM = 370.5;
+    }
+
+    public static class TagType
+    {
+        public const int Nil = 0x00;
+        public const int Boolean = 0x01;
+        public const int Number = 0x03;
+        public const int Integer = 0x13;
+        public const int ShortStr = 0x04;
+        public const int LongStr = 0x14;
+    }
+
+    //头部字段
+    public class Header
+    {
+        private uint signature;         //签名,不符合拒绝加载
+        private byte version;           //版本号(大版本号Mv、小版本号mv、发布号，例如：5.3.4。其值为Mv*16+mv)，不符合拒绝加载
+        private byte format;            //格式号(0)，不符合拒绝加载
+        private byte[] luacData;        //进一步校验，不符合拒绝加载
+        private byte cintSize;          //04
+        private byte sizetSize;         //08
+        private byte instructonSize;    //04
+        private byte luaIntegerSize;    //08
+        private byte luaNumberSize;     //08
+        private byte[] luacInt;         //n个字节存放lua整数值0x5678，所以这里n是8。目的是检查大小端
+        private byte[] luacNum;         //最后n个字节存放Lua浮点数370.5，所以这里n是8.目的是检测二进制chunk所使用的浮点数格式
+    }
+
+    //函数原型
+    public class Prototype
+    {
+        public string Source;           //源文件
+        public uint LineDefined;        //起止行号
+        public uint LastLineDefined;
+        public byte NumParams;          //固定参数个数
+        public byte IsVararg;           //是否是Vararg函数
+        public byte MaxStackSize;       //寄存器数量
+        public List<uint> Code;         //指令表
+        public List<object> Constants;      //常量表
+        public List<Upvalue> Upvalues;      //Upvalue表
+        public List<Prototype> Protos;      //子函数原型表
+        public List<uint> LineInfo;         //行号表
+        public List<LocVar> LocVars;        //局部变量表
+        public List<string> UpvalueNames;   //Upvalue名列表
+
+    }
+
+    public class Upvalue
+    {
+        public byte istack;
+        public byte idx;
+
+        public Upvalue(byte istack, byte idx)
+        {
+            this.istack = istack;
+            this.idx = idx;
+        }
+    }
+
+    public class LocVar
+    {
+        public string varName;
+        public uint startPC;
+        public uint endPC;
+
+        public LocVar(string varName, uint startPC, uint endPC)
+        {
+            this.varName = varName;
+            this.startPC = startPC;
+            this.endPC = endPC;
+        }
+    }
+
+    public class Binary_Chunk
+    {
+        private Header header;      // 头部
+        private int upvaluesSize;   // 主函数upvalue数量
+        private Prototype mainFunc; // 主函数原型
+
+        public static Prototype Undump(byte[] data)
+        {
+            var reader = new Reader(data);
+            reader.CheckHeader();       // 校验头部
+            reader.ReadByte();          // 跳过 Upvalue 数量
+            return reader.ReadProto(""); // 读取函数原型
+        }
+
+        public static void Print(Prototype f)
+        {
+
+            // 头部信息
+            string funcType = (f.LineDefined > 0) ? "function" : "main";
+            string varargFlag = (f.IsVararg > 0) ? "+" : "";
+
+            Console.WriteLine("\n{0} <{1}:{2}, {3}> ({4} instructions)",
+                funcType, f.Source, f.LineDefined, f.LastLineDefined, f.Code.Count);
+
+            Console.WriteLine("{0}{1} params, {2} slots, {3} upvalues, ",
+                f.NumParams, varargFlag, f.MaxStackSize, f.Upvalues.Count);
+
+            Console.WriteLine("{0} locals, {1} constants, {2} functions",
+                f.LocVars.Count, f.Constants.Count, f.Protos.Count);
+
+            // code
+            for (int pc = 0; pc < f.Code.Count; pc++)
+            {
+                string line = "-";
+                if (f.LineInfo != null && f.LineInfo.Count > pc)
+                {
+                    line = f.LineInfo[pc].ToString();
+                }
+
+                Console.WriteLine("\t{0}\t[{1}]\t0x{2:X8}", pc + 1, line, f.Code[pc]);
+            }
+            
+            // 常量表
+            Console.WriteLine("constants ({0}):", f.Constants.Count);
+            for (int i = 0; i < f.Constants.Count; i++)
+            {
+                Console.WriteLine("\t{0}\t{1}", i + 1, ConstantToString(f.Constants[i]));
+            }
+
+            Console.WriteLine("locals ({0}):", f.LocVars.Count);
+            for (int i = 0; i < f.LocVars.Count; i++)
+            {
+                var locVar = f.LocVars[i];
+                Console.WriteLine("\t{0}\t{1}\t{2}\t{3}", i, locVar.varName, locVar.startPC + 1, locVar.startPC + 1);
+            }
+
+            // upvalue
+            Console.WriteLine("upvalues ({0}):", f.Upvalues.Count);
+            for (int i = 0; i < f.Upvalues.Count; i++)
+            {
+                var upval = f.Upvalues[i];
+                Console.WriteLine("\t{0}\t{1}\t{2}\t{3}", i, UpvalName(f, i), upval.istack, upval.idx);
+            }
+
+
+
+        }
+
+
+        public static string UpvalName(Prototype f, int idx)
+        {
+            if (f.UpvalueNames != null && f.UpvalueNames.Count > 0)
+            {
+                return f.UpvalueNames[idx];
+            }
+            return "-";
+        }
+
+
+        public static string ConstantToString(object k)
+        {
+            switch (k)
+            {
+                case null:
+                    return "nil";
+                case bool b:
+                    return b.ToString().ToLower();
+                case double d:
+                    return d.ToString("G");
+                case long i:
+                    return i.ToString();
+                case string s:
+                    return $"\"{s}\"";
+                default:
+                    return "?";
+            }
+        }
+
+    }
+
+}
---
+++ MMO-SERVER/Llua/binchunk/Reader.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lLua.binchunk
+{
+    //具体的而记者chunk解析工作由reader来完成
+    public class Reader
+    {
+        private byte[] data;
+        private int position;
+        public Reader(byte[] data)
+        {
+            this.data = data;
+            this.position = 0;
+        }
+
+        public byte ReadByte()
+        {
+            if (position + 1 > data.Length)
+            {
+                throw new InvalidOperationException("Attempt to read beyond the end of the data.");
+            }
+
+            byte b = data[position];
+            position++;
+            return b;
+        }
+
+        public uint ReadUint32()
+        {
+            if (position + 4 > data.Length)
+            {
+                throw new InvalidOperationException("Attempt to read beyond the end of the data.");
+            }
+
+            uint value = BitConverter.ToUInt32(data, position);
+            position += 4;
+            return value;
+        }
+
+        public ulong ReadUint64()
+        {
+            if (position + 8 > data.Length)
+            {
+                throw new InvalidOperationException("Attempt to read beyond the end of the data.");
+            }
+
+            ulong value = BitConverter.ToUInt64(data, position);
+            position += 8;
+            return value;
+        }
+
+        public long ReadLuaInteger()
+        {
+            return (long)ReadUint64();
+        }
+
+        public double ReadLuaNumber()
+        {
+            ulong bits = ReadUint64();
+            return BitConverter.Int64BitsToDouble((long)bits);
+        }
+
+        public byte[] ReadBytes(uint n)
+        {
+            if (position + n > data.Length)
+            {
+                throw new InvalidOperationException("Attempt to read beyond the end of the data.");
+            }
+
+            byte[] bytes = new byte[n];
+            Array.Copy(data, position, bytes, 0, n);
+            position += (int)n;
+            return bytes;
+        }
+
+        public string ReadString()
+        {
+            uint size = ReadByte(); // Attempt to read a short string
+
+            if (size == 0) // Null string
+            {
+                return "";
+            }
+
+            if (size == 0xFF) // Long string
+            {
+                size = (uint)ReadUint64();
+            }
+
+            byte[] bytes = ReadBytes(size - 1); // Read the string bytes, excluding the terminator
+            return Encoding.UTF8.GetString(bytes); // Convert bytes to UTF-8 string
+        }
+
+        public void CheckHeader()
+        {
+            if (Encoding.ASCII.GetString(ReadBytes(4)) != LuaConstants.LUA_SIGNATURE)
+            {
+                throw new InvalidOperationException("not a precompiled chunk!");
+            }
+            else if (ReadByte() != LuaConstants.LUAC_VERSION)
+            {
+                throw new InvalidOperationException("version mismatch!");
+            }
+            else if (ReadByte() != LuaConstants.LUAC_FORMAT)
+            {
+                throw new InvalidOperationException("format mismatch!");
+            }
+            else if (!ReadBytes(6).SequenceEqual(LuaConstants.LUAC_DATA))
+            {
+                throw new InvalidOperationException("corrupted!");
+            }
+            else if (ReadByte() != LuaConstants.CINT_SIZE)
+            {
+                throw new InvalidOperationException("int size mismatch!");
+            }
+            else if (ReadByte() != LuaConstants.CSIZET_SIZE)
+            {
+                throw new InvalidOperationException("size_t size mismatch!");
+            }
+            else if (ReadByte() != LuaConstants.INSTRUCTION_SIZE)
+            {
+                throw new InvalidOperationException("instruction size mismatch!");
+            }
+            else if (ReadByte() != LuaConstants.LUA_INTEGER_SIZE)
+            {
+                throw new InvalidOperationException("lua_Integer size mismatch!");
+            }
+            else if (ReadByte() != LuaConstants.LUA_NUMBER_SIZE)
+            {
+                throw new InvalidOperationException("lua_Number size mismatch!");
+            }
+            else if (ReadLuaInteger() != LuaConstants.LUAC_INT)
+            {
+                throw new InvalidOperationException("endianness mismatch!");
+            }
+            else if (ReadLuaNumber() != LuaConstants.LUAC_NUM)
+            {
+                throw new InvalidOperationException("float format mismatch!");
+            }
+        }
+
+        public Prototype ReadProto(string parentSource)
+        {
+            string source = ReadString();
+            if (string.IsNullOrEmpty(source))
+            {
+                source = parentSource;
+            }
+
+            return new Prototype
+            {
+                Source = source,
+                LineDefined = ReadUint32(),
+                LastLineDefined = ReadUint32(),
+                NumParams = ReadByte(),
+                IsVararg = ReadByte(),
+                MaxStackSize = ReadByte(),
+                Code = ReadCode(),
+                Constants = ReadConstants(),
+                Upvalues = ReadUpvalues(),
+                Protos = ReadProtos(source),
+                LineInfo = ReadLineInfo(),
+                LocVars = ReadLocVars(),
+                UpvalueNames = ReadUpvalueNames()
+            };
+        }
+
+        public List<uint> ReadCode()
+        {
+            // 创建一个大小为 ReadUint32 返回值的 uint 列表
+            int size = (int)ReadUint32();  // 从字节流中读取长度
+            List<uint> code = new List<uint>(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                // 逐个读取 uint 值并加入列表
+                code.Add(ReadUint32());
+            }
+
+            return code;
+        }
+
+        public List<object> ReadConstants()
+        {
+            // 创建一个大小为 ReadUint32 返回值的 object 列表
+            int size = (int)ReadUint32();  // 从字节流中读取常量表的长度
+            List<object> constants = new List<object>(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                // 读取常量并加入列表
+                constants.Add(ReadConstant());
+            }
+
+            return constants;
+        }
+
+        public object ReadConstant()
+        {
+            byte tag = ReadByte(); // 获取标签
+
+            switch (tag)
+            {
+                case TagType.Nil:
+                    return null; // 返回 null
+
+                case TagType.Boolean:
+                    return ReadByte() != 0; // 返回布尔值
+
+                case TagType.Integer:
+                    return ReadLuaInteger(); // 返回整数
+
+                case TagType.Number:
+                    return ReadLuaNumber(); // 返回浮点数
+
+                case TagType.ShortStr:
+                case TagType.LongStr:
+                    return ReadString(); // 返回字符串
+
+                default:
+                    throw new InvalidOperationException("Corrupted!"); // 抛出异常
+            }
+        }
+
+        public List<Upvalue> ReadUpvalues()
+        {
+            uint count = ReadUint32();
+            var upvalues = new List<Upvalue>((int)count);
+
+            for (int i = 0; i < count; i++)
+            {
+                byte instack = ReadByte();
+                byte idx = ReadByte();
+                upvalues.Add(new Upvalue(instack, idx));
+            }
+            return upvalues;
+        }
+
+        public List<Prototype> ReadProtos(string parentSource)
+        {
+            uint count = ReadUint32();
+            var protos = new List<Prototype>((int)count);
+
+            for (int i = 0; i < count; i++)
+            {
+                protos.Add(ReadProto(parentSource));
+            }
+
+            return protos;
+        }
+
+        public List<uint> ReadLineInfo()
+        {
+            uint count = ReadUint32();
+            var lineInfo = new List<uint>((int)count);
+
+            for (int i = 0; i < count; i++)
+            {
+                lineInfo.Add(ReadUint32());
+            }
+
+            return lineInfo;
+        }
+
+        public List<LocVar> ReadLocVars()
+        {
+            uint count = ReadUint32();
+            var locVars = new List<LocVar>((int)count);
+
+            for (int i = 0; i < count; i++)
+            {
+                string varName = ReadString();
+                uint startPC = ReadUint32();
+                uint endPC = ReadUint32();
+                locVars.Add(new LocVar(varName, startPC, endPC));
+            }
+
+            return locVars;
+        }
+
+        public List<string> ReadUpvalueNames()
+        {
+            uint count = ReadUint32();
+            var names = new List<string>((int)count);
+
+            for (int i = 0; i < count; i++)
+            {
+                names.Add(ReadString());
+            }
+
+            return names;
+        }
+
+    }
+}
