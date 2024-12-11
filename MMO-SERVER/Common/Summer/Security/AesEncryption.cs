9e22724082ac4e5edf54ac3c7f759d5c656675d2
大衍神君且刮风 <2528201418@qq.com>
2024-12-11 21:03:08 +0800

    lLua测试

---
+++ MMO-SERVER/Common/Summer/Security/AesEncryption.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Summer.Security
+{
+    public class AesEncryption
+    {
+        private readonly byte[] key;
+        private readonly byte[] iv;
+
+        public AesEncryption(string Key,string IV)
+        {
+            // Ensure the key and IV have a length of 16 bytes (128 bits), 24 bytes (192 bits), or 32 bytes (256 bits).
+            key = Encoding.UTF8.GetBytes(Key.PadRight(32).Substring(0, 32));
+            iv = Encoding.UTF8.GetBytes(IV.PadRight(16).Substring(0, 16));
+        }
+
+        public string Encrypt(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+        public string Decrypt(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+
+        public static (string Key, string IV) GenerateAesKeyAndIv()
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateKey();
+                aes.GenerateIV();
+
+                // Convert the byte arrays to Base64 strings for easy storage or transmission
+                string key = Convert.ToBase64String(aes.Key);
+                string iv = Convert.ToBase64String(aes.IV);
+
+                return (key, iv);
+            }
+        }
+    }
+}
---
+++ MMO-SERVER/Common/Summer/Security/RsaEncryption.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Summer.Security
+{
+    public class RsaEncryption
+    {
+        private RSA rsa;
+
+        public RsaEncryption()
+        {
+            rsa = RSA.Create();
+        }
+
+        public string GetPublicKey()
+        {
+            return Convert.ToBase64String(rsa.ExportSubjectPublicKeyInfo());
+        }
+
+        public void ImportPublicKey(string publicKey)
+        {
+            rsa.ImportSubjectPublicKeyInfo(Convert.FromBase64String(publicKey), out _);
+        }
+
+        public string GetPrivateKey()
+        {
+            return Convert.ToBase64String(rsa.ExportPkcs8PrivateKey());
+        }
+
+        public void ImportPrivateKey(string privateKey)
+        {
+            rsa.ImportPkcs8PrivateKey(Convert.FromBase64String(privateKey), out _);
+        }
+
+        public string Encrypt(string plainText)
+        {
+            byte[] dataToEncrypt = Encoding.UTF8.GetBytes(plainText);
+            byte[] encryptedData = rsa.Encrypt(dataToEncrypt, RSAEncryptionPadding.Pkcs1);
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            byte[] dataToDecrypt = Convert.FromBase64String(cipherText);
+            byte[] decryptedData = rsa.Decrypt(dataToDecrypt, RSAEncryptionPadding.Pkcs1);
+            return Encoding.UTF8.GetString(decryptedData);
+        }
+
+        public static (string PublicKey, string PrivateKey) GenerateRsaKeyPair(int keySize = 2048)
+        {
+            using (var rsa = RSA.Create())
+            {
+                rsa.KeySize = keySize;
+
+                // Export the keys in XML format
+                string publicKey = rsa.ToXmlString(false); // Only public key
+                string privateKey = rsa.ToXmlString(true); // Both public and private key
+
+                return (publicKey, privateKey);
+            }
+        }
+    }
+}
