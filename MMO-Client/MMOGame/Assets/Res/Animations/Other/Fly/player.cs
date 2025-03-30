4adb50ef242d016ee3738b543126f3188f34c09b
xiaoliangba <2528201418@qq.com>
2025-03-30 18:12:15 +0800

    [更新]客户端资源更新

---
+++ MMO-Client/MMOGame/Assets/Res/Animations/Other/Fly/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("1")) 
+		{
+			anim.Play("idle_ground", -1);
+		}
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			anim.Play("attack1", -1,0f);
+		}
+	}
+}
